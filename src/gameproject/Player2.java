0c88f08fe400dd455d80169de280180c82bdda98
Sirril <sirril29a@gmail.com>
2017-05-19 20:48:59 +0700

    Update Player2.java

--- src/gameproject/Player2.java
+++ src/gameproject/Player2.java
@@ -3,10 +3,7 @@ public class Player2 extends Player{
     
     public Player2(String nm, double gold, double gem) {
         super(nm, gold, gem);
-        
-        nm = "Sirril";
-        gold = 1450000;
-        gem = 450;
+    
     }
     
 }
