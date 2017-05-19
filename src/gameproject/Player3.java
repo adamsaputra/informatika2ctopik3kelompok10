70edd44f136e734354e46fad8381025837e618a4
Sirril <sirril29a@gmail.com>
2017-05-19 20:05:09 +0700

    Update Player3.java

--- src/gameproject/Player3.java
+++ src/gameproject/Player3.java
@@ -3,6 +3,10 @@ public class Player3 extends Player{
     
     public Player3(String nm, double gold, double gem) {
         super(nm, gold, gem);
+        
+        nm = "Diny";
+        gold = 1350000;
+        gem = 545;
     }
     
 }
