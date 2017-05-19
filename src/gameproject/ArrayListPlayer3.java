350a4e1a4d0161bf8ba64a689d81ae522dd78453
Sirril <sirril29a@gmail.com>
2017-05-19 20:56:47 +0700

    Create ArrayListPlayer3.java

---
+++ src/gameproject/ArrayListPlayer3.java
@@ -0,0 +1,15 @@
+package gameproject;
+
+import java.util.ArrayList;
+
+public class ArrayListPlayer3{
+
+  public static void main (String[]args){
+    ArrayList<player3> member = new ArrayList<>();
+    member.add(new Player3("Alan",3400000,540));
+    member.add(new Player3("Nuril",3450000,545));
+    member.add(new Player3("Rahendra",3500000,650));
+    member.add(new Player3("Geo",4500000,770));
+    member.add(new Player3("Iqbal",3500000,650));
+  }
+}
