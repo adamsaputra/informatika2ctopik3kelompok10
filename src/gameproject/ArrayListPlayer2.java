b422820630d07563cb883a85b4c4099d5ae93a78
twdini <triwahyu.diningrum@yahoo.com>
2017-05-19 23:00:12 +0700

    Update ArrayListPlayer2.java

--- src/gameproject/ArrayListPlayer2.java
+++ src/gameproject/ArrayListPlayer2.java
@@ -6,10 +6,10 @@ public class ArrayListPlayer2{
 
 public static void main (String[]args){
     ArrayList <Player2> member = new ArrayList<>();  
-    member.add(new Player2("Ridhi",4500000,245));
-    member.add(new Player2("Amril",3250000,350));
-    member.add(new Player2("Erlangga",5400000,150));
-    member.add(new Player2("Andi",3400000,540));
-    member.add(new Player2("Fuad",2450000,660));
+    member.add(new Player2("Adam",4500000,245));
+    member.add(new Player2("Sirril",3250000,350));
+    member.add(new Player2("Asty",5400000,150));
+    member.add(new Player2("Dini",3400000,540));
+    member.add(new Player2("Gondes",2450000,660));
 }
 }
