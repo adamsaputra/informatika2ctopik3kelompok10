24c97e6e259f6923748714139b34c6efed86a241
twdini <triwahyu.diningrum@yahoo.com>
2017-05-19 23:03:58 +0700

    Create ArrayListPlayer5.java

---
+++ src/gameproject/ArrayListPlayer5.java
@@ -0,0 +1,15 @@
+package gameproject;
+
+import java.util.ArrayList;
+
+public class ArrayListPlayer5{
+
+public static void main (String[]args){
+    ArrayList <Player5> member = new ArrayList<>();  
+    member.add(new Player5("Adam",5500000,157));
+    member.add(new Player5("Sirril",45665000,158));
+    member.add(new Player5("Asty",56770000,148));
+    member.add(new Player5(Dini",57700000,150));
+    member.add(new Player5("Gondes",2850000,888));
+}
+}
