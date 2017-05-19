267da566c13055386bdb4cd7e74dc43bea67ecaa
Sirril <sirril29a@gmail.com>
2017-05-19 20:44:36 +0700

    Create ArrayListPlayer2.java

---
+++ src/gameproject/ArrayListPlayer2.java
@@ -0,0 +1,15 @@
+package gameproject;
+
+import java.util.ArrayList;
+
+public class ArrayListPlayer2{
+
+public static void main (String[]args){
+    ArrayList <Player2> member = new ArrayList<>();  
+    member.add(new Player2("Ridhi",4500000,245));
+    member.add(new Player2("Amril",3250000,350));
+    member.add(new Player2("Erlangga",5400000,150));
+    member.add(new Player2("Andi",3400000,540));
+    member.add(new Player2("Fuad",2450000,660));
+}
+}
